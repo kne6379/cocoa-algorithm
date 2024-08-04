709f35cabba2e4e50b6f49b5a48190dabfd1dd87
Robbie <Robbie@email.com>
2024-08-04 18:58:00 +0900

    [Programmers-7] 예상 대진표

---
+++ Programmers/noeul/7_week/예상 대진표.js
@@ -0,0 +1,15 @@
+function solution(n, a, b) {
+  let answer = 0;
+
+  const recursion = (a, b, answer) => {
+    if (a !== b) {
+      a = Math.ceil(a / 2);
+      b = Math.ceil(b / 2);
+      answer++;
+      return recursion(a, b, answer);
+    }
+    return answer;
+  };
+  answer = recursion(a, b, answer);
+  return answer;
+}
